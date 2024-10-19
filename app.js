d882c8c06aa8f40d3d548f03fb997047bdcdc4ed
prasunTimalsina <timalsinaprasun61@gmail.com>
2024-10-19 11:07:28 +0545

    Add function to add book to the list using form

--- app.js
+++ app.js
@@ -53,5 +53,19 @@ class UI {
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 //Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  //Prevent actual submit
+  e.preventDefault();
 
+  // Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  //Add book to UI
+  UI.addBookToList(book);
+});
 //Event: Remove a Book
